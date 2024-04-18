0fad5ae3ffb195d6168d945a7be297ba64beb94b
Dany Meyer <dany.meyer@hnu.de>
2024-04-18 09:17:18 +0200

    add Kommentar

--- src/Testclass.java
+++ src/Testclass.java
@@ -1,9 +1,8 @@
 public class Testclass{
 
   public static void main (String[] args){
+	  //Das ist ein Kommentar
       System.out.println("Hello, there!");
-      
-      // das ist ein Kommentar
   }
 
 }
